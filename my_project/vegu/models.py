1773ae0299abfec3ccc19eb9004f880d13e3dd4d
jagernashrestha <jagerna1543@gmail.com>
2026-05-06 17:22:11 +0545

    Updated views, templates, and added new apps

---
+++ my_project/vegu/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+    recipe_image = models.ImageField(upload_to="recipie/", null = True, blank = True)
+
+    def __str__(self):
+        return self.name
---
+++ my_project/vegu/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import *
+# Create your views here.
+def recipe(request):
+    if request.method == "POST":
+        data = request.POST
+        name = data.get('name')
+        recipe_image = request.FILES.get("recipe_image")
+        description = data.get('description')
+        
+        Recipe.objects.create(
+            name = name,
+            description = description,
+            recipe_image = recipe_image
+        )
+        return redirect  ('/recipe/')
+    queryset = Recipe.objects.all()
+    context = {"recipe":queryset}
+    return render(request, "recipe.html", context )
